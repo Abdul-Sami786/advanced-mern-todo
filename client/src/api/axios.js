998d1cb4a6d8d269ec70625f6e391274e4adb4e3
Abdul Sami <gamingemperor@gmail.com>
2026-01-05 19:55:02 +0500

    Configure frontend for Railway backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base URL
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 // Add token to request headers if exists
