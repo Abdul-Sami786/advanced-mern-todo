dcefac249bc0f7d49b779c035bf9a42511a54c8c
Abdul Sami <gamingemperor@gmail.com>
2026-01-05 19:54:38 +0500

    Configure frontend for Railway backend

--- server/server.js
+++ server/server.js
@@ -13,8 +13,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://your-vercel-app.vercel.app"
+    "http://localhost:5173"
   ],
   credentials: true
 }));
